201ddc21e83320f870eb779fb7e4e5855b90c64e
Basliel <baslielz727@gmail.com>
2024-12-03 16:14:29 +0200

    Integrated displaybycategory functionality and styling

--- main.js
+++ main.js
@@ -31,8 +31,81 @@ async function getData(){
     
 }
 
+document.getElementById('men-btn').addEventListener('click', menCategory)
 
 
+ async function menCategory(){
+    data = await fetch('https://fakestoreapi.com/products')
+    myData = await data.json()
+    console.log(myData)
+    productContainer = document.getElementById('product-container')
+    productContainer.innerHTML = ''
+    for (i = 0;i<myData.length; i++){
+        console.log(myData[i].category)
+        if (myData[i].category == "men's clothing"){
+            product = document.createElement('div')
+            product.classList.add('product-card')
+            product.innerHTML += `
+            <img class="product-card-image" src="${myData[i].image}" alt="">
+            <h3 class= "product-card-title">${myData[i].title}</h3>
+            <p class="product-card-price">${myData[i].price}</p>
+            <button class="addToCart-btn" onclick = "addToCart()">Add to Cart</button>`
+            productContainer.appendChild(product)
+
+        }
+    }
+}
+
+
+document.getElementById('women-btn').addEventListener('click', womenCategory)
+
+
+ async function womenCategory(){
+    data = await fetch('https://fakestoreapi.com/products')
+    myData = await data.json()
+    console.log(myData)
+    productContainer = document.getElementById('product-container')
+    productContainer.innerHTML = ''
+    for (i = 0;i<myData.length; i++){
+        console.log(myData[i].category)
+        if (myData[i].category == "women's clothing"){
+            product = document.createElement('div')
+            product.classList.add('product-card')
+            product.innerHTML += `
+            <img class="product-card-image" src="${myData[i].image}" alt="">
+            <h3 class= "product-card-title">${myData[i].title}</h3>
+            <p class="product-card-price">${myData[i].price}</p>
+            <button class="addToCart-btn" onclick = "addToCart()">Add to Cart</button>`
+            productContainer.appendChild(product)
+
+        }
+    }
+}
+
+document.getElementById('electronics-btn').addEventListener('click', accessoriesCategory)
+
+
+async function accessoriesCategory(){
+   data = await fetch('https://fakestoreapi.com/products')
+   myData = await data.json()
+   console.log(myData)
+   productContainer = document.getElementById('product-container')
+   productContainer.innerHTML = ''
+   for (i = 0;i<myData.length; i++){
+       console.log(myData[i].category)
+       if (myData[i].category == "electronics" || myData[i].category == 'jewelery'){
+           product = document.createElement('div')
+           product.classList.add('product-card')
+           product.innerHTML += `
+           <img class="product-card-image" src="${myData[i].image}" alt="">
+           <h3 class= "product-card-title">${myData[i].title}</h3>
+           <p class="product-card-price">${myData[i].price}</p>
+           <button class="addToCart-btn" onclick = "addToCart()">Add to Cart</button>`
+           productContainer.appendChild(product)
+
+       }
+   }
+}
 
 getData()
 
