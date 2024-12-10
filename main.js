6b0154c42806d805d0e6b27688426df64119a27e
Basliel <baslielz727@gmail.com>
2024-12-10 17:16:40 +0200

    added the send order functionality

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ products = [] //Initialise an empty list for future product objects
 
 
 async function getData(){
+    products = []
     data = await fetch('https://fakestoreapi.com/products') //fetch api and convert a useable JS array
     myData = await data.json()
     for (i = 0; i<myData.length; i++){ //Loop through each element in fetched data and add it to empty array
@@ -14,7 +15,6 @@ async function getData(){
     displayProducts()
 }
 
-getData()
 
 
 function displayProducts(){ 
@@ -32,14 +32,14 @@ function displayProducts(){
         <h3 class= "product-card-title">${products[i].title}</h3>
         <p class="product-card-description">${products[i].description}</p>
         <p class="product-card-price">${products[i].price}</p>
-        <button class="addToCart-btn" onclick = "addToCart(${myData[i].id})">Add to Cart</button>`//Adding contents into product card
+        <button class="addToCart-btn" onclick = "addToCart(${products[i].id})">Add to Cart</button>`//Adding contents into product card
         document.getElementById('product-container').appendChild(product) //product card is inserted in a bigger container for organized display
-
         
     }
     
 }
 
+getData()
 
 //Function for displaying the 'Men' category products
 document.getElementById('men-btn').addEventListener('click', menCategory)
@@ -180,8 +180,9 @@ function showCart(){
         <div class="count-btn"><button id="addCount-btn" onclick="addCount(${productsInCart[i].id})">+</button><p class="" id="count${productsInCart[i].id}">${productsInCart[i].count}</p><button id="subtractCount-btn" onclick="subtractCount(${productsInCart[i].id})">-</button></div>
         <button onclick="removeProduct(${productsInCart[i].id})" class="remove-btn">Remove</button>`//Adding contents to product card
 
+        document.getElementById('totalprice-container').innerHTML= `Total: ${totalprice.toFixed(2)} <button class="send-order-btn" onclick="sendOrder()">Send Order</button>`
         productContainer.appendChild(product)
-        document.getElementById('totalprice-container').innerHTML= `Total: ${totalprice.toFixed(2)}`
+        resetStyle()
     }
 }
 
@@ -194,7 +195,7 @@ function addCount(id){
             newCount = count +1 
             productsInCart[i].count = newCount
             totalprice += productsInCart[i].price
-            document.getElementById('totalprice-container').innerHTML = `Total: ${totalprice.toFixed(2)}`
+            document.getElementById('totalprice-container').innerHTML = `Total: ${totalprice.toFixed(2)} <button class="send-order-btn" onclick="sendOrder()">Send Order</button>`
             console.log(totalprice)
             document.getElementById(`count${productsInCart[i].id}`).innerText = `${productsInCart[i].count}`
             console.log(productsInCart[i].count)
@@ -212,7 +213,7 @@ function subtractCount(id){
                 newCount = count - 1
                 productsInCart[i].count = newCount
                 totalprice -= productsInCart[i].price
-                document.getElementById('totalprice-container').innerHTML = `Total: ${totalprice.toFixed(2)}`
+                document.getElementById('totalprice-container').innerHTML = `Total: ${totalprice.toFixed(2)} <button class="send-order-btn" onclick="sendOrder()">Send Order</button>`
                 console.log(totalprice)
                 document.getElementById(`count${productsInCart[i].id}`).innerText = `${productsInCart[i].count}`
     
@@ -236,6 +237,57 @@ function removeProduct(id){
 }
 
 
+function sendOrder(){
+    console.log(productsInCart)
+    content = document.getElementById('product-container')
+    content.style.display = 'flex'
+    content.innerHTML = `<div class="checkout">
+        <input id= "name" type="text" placeholder="Name">
+        <input id= "address" type="text" placeholder="Address">
+        <input id= "cardnumber" type="text" placeholder="Card Number: XXXX-XXXX-XXXX-XXXX">
+        <input id= "phonenumber" type="tel" placeholder="Phone Number">
+        <p>Price: ${totalprice.toFixed(2)}</p>
+        <button onclick = "completeOrder()">Send</button>
+    </div>`
+    document.getElementById('totalprice-container').style.display = 'none'
+
+
+}
+
+
+orders = []
+
+function Order(name, address, cardNumber, phoneNumber){
+    this.order = productsInCart
+    this.name = name
+    this.address = address
+    this.cardNumber = cardNumber
+    this.phoneNumber = phoneNumber
+}
+
+function completeOrder(){
+    customerName = document.getElementById('name').value
+    address = document.getElementById('address').value
+    cardNumber = document.getElementById('cardnumber').value
+    phoneNumber = document.getElementById('phonenumber').value
+
+    order = new Order(customerName, address, cardNumber, phoneNumber)
+    orders.push(order)
+
+    console.log(order)
+    console.log(orders)
+
+
+    document.getElementById('product-container').innerHTML = '<p>You order has been sent</p>'
+    products = []
+    productsInCart = []
+    totalprice = 0
+}
+
+function resetStyle(){
+    document.getElementById('totalprice-container').style= ''
+    document.getElementById('product-container').style = ''
+}
 
 array = ['a', 'b', 'c', 'd']
 
